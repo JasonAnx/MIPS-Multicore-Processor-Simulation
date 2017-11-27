b31e35206a9fa31fde3e2b54d678dd6978afedf5
Jason Anchia <jasonanchia@hotmail.com>
2017-11-26 21:11:55 -0600

    added try-catch to user input

--- ProyectoArqui/UserInterface.cs
+++ ProyectoArqui/UserInterface.cs
@@ -3,27 +3,26 @@ namespace ProyectoArqui
 {
     public class UserInterface
     {
-        public UserInterface()
-        {
-        }
-           
-        // Initialize user interface
-        public void init(){
-            
-            
-        }
-
         // Ask for user quantum and return it
 
-        public int getUserQuantum(){
-            Console.WriteLine("Enter desired user quantum: ");
-            int userQuantum = Convert.ToInt32(Console.ReadLine());
-            return userQuantum;
+        public uint getUserQuantum()
+        {
+            Console.WriteLine("Enter desired quantum: ");
+            try
+            {
+                uint userQuantum = Convert.ToUInt32(Console.ReadLine());
+                return userQuantum;
+            }
+            catch (ArgumentException) { OperatingSystem.logError(" ArgumentException ", true); }
+            catch (OverflowException) { OperatingSystem.logError(" Overflow: number is too big or negative", true); }
+            catch (FormatException) { OperatingSystem.logError(" FormatException: could not convert input to a valid number ", true); }
+            return 0;
         }
 
         // Ask if user wants to run simulation in slow mode
 
-        public bool getSlowModeActivated(){
+        public bool getSlowModeActivated()
+        {
             string slowMode = "";
             bool userSlowMode = false; // Default value: 0
             while (slowMode != "Y" && slowMode != "y" && slowMode != "N" && slowMode != "n")
